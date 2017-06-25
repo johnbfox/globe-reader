4e7eab0ecf8da41096d3041872490b0bbb861e54
John Fox <johnfox@Johns-MacBook-Pro.local>
2017-06-25 10:26:25 -0400

    Initial commit

---
+++ inject.js
@@ -0,0 +1,10 @@
+
+(function() {
+
+  document.querySelector('html').style['overflow'] = 'auto';
+  document.querySelector('.mfp-wrap').style['display'] = 'none';
+  document.querySelector('.mfp-bg').style['display'] = 'none';
+  document.querySelector('#contain').style['overflow'] = 'auto';
+  document.querySelector('#contain').style['height'] = 'auto';
+
+})();
